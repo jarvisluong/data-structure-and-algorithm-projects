4003fbf6c619c31adb72fd2358a98f599a3c7669
Luong Dang Hai <hailuongdang1998@gmail.com>
2017-04-24 11:02:53 +0300

    Small changes to hw3 (compulsory parts)

--- hw3/datastructure.hpp
+++ hw3/datastructure.hpp
@@ -9,6 +9,25 @@ using std::string;
 #include <vector>
 using std::vector;
 
+#include <deque>
+using std::deque;
+
+#include <queue>
+using std::queue;
+
+#include <map>
+using std::map;
+using std::multimap;
+
+#include <unordered_map>
+using std::unordered_map;
+
+#include <set>
+using std::set;
+
+#include <unordered_set>
+using std::unordered_set;
+
 #include <utility>
 using std::pair;
 
@@ -20,6 +39,21 @@ Salary const NO_SALARY = -1;
 
 using Cost = int;
 Cost const NO_COST = -1;
+
+struct Person {
+        PersonID id;
+        string name;
+        Salary salary;
+        string title;
+
+        bool leader = false;
+        PersonID bossid = NO_ID;
+        vector<PersonID> underlings;
+
+        unordered_map<PersonID, Cost> friends;
+        int color = 0;
+        PersonID nearest_friend = NO_ID;
+};
 
 class Datastructure
 {
@@ -45,6 +79,7 @@ public:
     void clear();
 
     vector<PersonID> underlings(PersonID id);
+    vector<PersonID> all_underlings(vector<PersonID>& ids);
     vector<PersonID> personnel_alphabetically();
     vector<PersonID> personnel_salary_order();
 
@@ -54,6 +89,9 @@ public:
 
     PersonID min_salary();
     PersonID max_salary();
+
+    // Find all the bosses (direct and indirect) of an ID
+    deque<PersonID> find_bosses(PersonID id);
 
     PersonID median_salary();
     PersonID first_quartile_salary();
@@ -67,12 +105,20 @@ public:
 
     bool check_boss_hierarchy();
     vector<pair<PersonID, Cost>> shortest_friendpath(PersonID fromid, PersonID toid);
+    vector<pair<PersonID, Cost>> shortest_friendpath_helper(PersonID fromid, PersonID toid);
     vector<pair<PersonID, Cost>> cheapest_friendpath(PersonID fromid, PersonID toid);
     pair<unsigned int, Cost> leave_cheapest_friendforest();
 
 private:
-    // Add your implementation here
+    // This map is used to find name quickly, and use the alphalist quick
+    // (map and set have been already sorted)
+    map<string, set<PersonID> > name_dict;
 
+    // We don't need to sort through salary since we got this
+    multimap<Salary, PersonID> salary_dict;
+
+    // Since we need to find person by ID alot then this unordered_map is ideal
+    unordered_map<PersonID, Person> id_dict;
 };
 
 #endif // DATASTRUCTURE_HPP
